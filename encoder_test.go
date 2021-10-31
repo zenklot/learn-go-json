11785d458127f14fbee411f59ea2305fe0ed5acb
zenklot <zenklot11@gmail.com>
2021-10-31 22:24:00 +0700

    encoder

---
+++ encoder_test.go
@@ -0,0 +1,20 @@
+package learngojson
+
+import (
+	"encoding/json"
+	"os"
+	"testing"
+)
+
+func TestEncoder(t *testing.T) {
+	writer, _ := os.Create("CustomerOut.json")
+	encoder := json.NewEncoder(writer)
+
+	customer := Customer{
+		FirstName:  "Eko",
+		MiddleName: "Kurniawan",
+		LastName:   "Khannedy",
+	}
+
+	encoder.Encode(customer)
+}
